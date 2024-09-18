580df7e2458e0e7483d4536c69936246e0dde677
k1r1k1 <kirillmerkulov@tut.by>
2024-09-18 21:17:11 +0300

    authorization

---
+++ src/app.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import jwt from 'jsonwebtoken'
+
+import usersRoutes from './routes/users.js'
+
+// config
+
+const App = express()
+App.use(bodyParser.urlencoded({ extended: false }))
+App.use(bodyParser.json())
+
+// check auth
+
+App.use((req, res, next) => {
+  if (req.url === '/login' || req.url === '/register') return next()
+  if (!req.headers?.authorization) {
+    return res.status(401).json({
+      message: 'unauthorized'
+    })
+  } else {
+      jwt.verify(
+          req.headers.authorization.split(' ')[1],
+          process.env.APP_SECRET,
+          (err, payload) => {
+              if (err) {
+                return res.status(401).json({
+                  message: err.message
+                })
+              }
+              if (payload) {
+                req.user = payload
+                next()
+              }
+          }
+      )
+  }
+})
+
+// routes
+
+App.get('/healthcheck', function(req, res) {
+  res.json({
+    status: 'running'
+  });
+});
+
+App.use(usersRoutes)
+
+App.listen(3000, () => {
+  console.log('server listening on: 3000')
+})
---
+++ src/routes/users.js
@@ -0,0 +1,141 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import Connection from '../sqlConnection.js'
+
+const router = express.Router()
+
+const isLoginPasswordInvalid = (str) =>
+  (/[^a-z0-9_.@]/i).test(str)
+
+router.post('/register', async (req, res) => {
+  try {
+    const { password, username } = req.body
+    if (!password || !username) {
+      return res.status(400).json({
+        message: 'No username or password'
+      })
+    }
+
+    if (password.length < 3 || isLoginPasswordInvalid(password)) {
+      return res.status(400).json({
+        message: 'Password must be at least 3 characters long, letters, numbers or symbols _ . @'
+      })
+    }
+
+    if (username.length < 3 || isLoginPasswordInvalid(username)) {
+      return res.status(400).json({
+        message: 'Username must be at least 3 characters long, letters, numbers or symbols _ . @'
+      })
+    }
+
+    Connection.query('SELECT * FROM users', async function (err, result) {
+      if (err) throw err
+      const foundUser = result.find(user => user.username === username)
+
+      if (foundUser) {
+        res.status(403).json({
+          message: 'User exists'
+        })
+      } else {
+        try {
+          await Connection.promise().query(
+            `INSERT INTO users (username, password) 
+          VALUES (?,?)`,
+            [username, password]
+          )
+          res.status(201).json({
+            message: 'User Created',
+          })
+        } catch (err) {
+          console.error(err)
+          res.status(500).json({
+            message: err,
+          })
+        }
+      }
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      message: err,
+    })
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const { password, username } = req.body
+    if (!password || !username) {
+      return res.status(400).json({
+        message: 'No username or password'
+      })
+    }
+
+    if (password.length < 3 || isLoginPasswordInvalid(password)) {
+      return res.status(400).json({
+        message: 'Password must be at least 3 characters long, letters, numbers or symbols _ . @'
+      })
+    }
+
+    if (username.length < 3 || isLoginPasswordInvalid(username)) {
+      return res.status(400).json({
+        message: 'Username must be at least 3 characters long, letters, numbers or symbols _ . @'
+      })
+    }
+
+    Connection.query('SELECT * FROM users', async function (err, result) {
+      if (err) throw err
+      const foundUser = result.find(user => user.username === username)
+
+      if (foundUser) {
+        if (foundUser.password !== password) {
+          return res.status(400).json({
+            message: 'Incorrect password'
+          })
+        }
+
+        const JWTToken = jwt.sign(
+          {
+            username: foundUser.username,
+            id: foundUser.id
+          },
+          process.env.APP_SECRET,
+          {
+            expiresIn: '1m'
+          }
+        )
+
+        res.status(200).json({
+          token: JWTToken,
+          user: foundUser
+        })
+      } else {
+        res.status(404).json({
+          message: 'User not found'
+        })
+      }
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      message: err,
+    })
+  }
+})
+
+router.get('/user', async (req, res) => {
+  try {
+    const { username, id } = req.user
+    res.status(200).json({
+      username,
+      id,
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      message: err,
+    })
+  }
+})
+
+export default router
